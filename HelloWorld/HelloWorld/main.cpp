874edd07c8638d2689545b404da7ca44fcb69711
chibao <397834690@qq.com>
2018-05-17 12:22:55 -0400

    Add HelloWorld

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by 吃饱饱 on 5/17/18.
+//  Copyright © 2018 chibao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
